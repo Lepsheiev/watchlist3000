ed9b138a8e5c66f26b00df21a1eaf07052646dec
Lepsheiev <108818332+Lepsheiev@users.noreply.github.com>
2026-03-30 11:51:33 +0300

    Update watchlist-public.js

--- netlify/functions/watchlist-public.js
+++ netlify/functions/watchlist-public.js
@@ -1,6 +1,6 @@
 exports.handler = async () => {
   const res = await fetch(
-    'https://znpmxqjkyxlanqbyflyy.supabase.co/rest/v1/watchlist?select=data&limit=1',
+    `https://znpmxqjkyxlanqbyflyy.supabase.co/rest/v1/watchlist?select=data&limit=1&t=${Date.now()}`,
     {
       headers: {
         'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpucG14cWpreXhsYW5xYnlmbHl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxNTU0NjcsImV4cCI6MjA4OTczMTQ2N30.noKaNHgmE4IJfWHVwuiK2_Z5yuEXG0JN_rlmrvG8al4',
